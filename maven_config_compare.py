60c6bca1e69faf69045a1825595ef2c2332ee6d2
dblue <likjjdblue@gmail.com>
2018-02-21 00:31:23 +0800

    Initial commit

---
+++ maven_config_compare.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from os import walk
+from os.path import isdir,isfile,abspath,join
+import os
+import re
+import codecs
+
+class MavenConfigDiffer:
+    def __init__(self,srcpath='source/',targetpath='target/'):
+        if not isdir(srcpath):
+            raise Exception('文件夹路径不存在:'+str(srcpath))
+        if not isdir(targetpath):
+            raise Exception('文件夹路径不存在:'+str(targetpath))
+
+        self.BaseSrcDir=srcpath
+        self.BaseTargetDir=targetpath
+
+    def differFileContent(self,fileA,fileB):
+        with codecs.open(fileA,'r','utf-8') as f:
+            FileContentA=f.read()
+        with codecs.open(fileB,'r','utf-8') as f:
+            FileContentB=f.read()
+
+        TmpDictA={}
+        for item in re.findall(r'^\s*[^#]*?$',FileContentA,flags=re.UNICODE|re.MULTILINE):
+            TmpList=item.split('=')
+            if len(TmpList)>=2:
+                TmpKey,TmpValue=TmpList[0].strip(),''.join(TmpList[1:]).strip()
+                TmpDictA.setdefault(TmpKey,TmpValue)
+
+        TmpDictB={}
+        for item in re.findall(r'^\s*[^#]*?$',FileContentB,flags=re.UNICODE|re.MULTILINE):
+            TmpList=item.split('=')
+            if len(TmpList)>=2:
+                TmpKey,TmpValue=TmpList[0].strip(),''.join(TmpList[1:]).strip()
+                TmpDictB.setdefault(TmpKey,TmpValue)
+        TmpDifferA=TmpDictA.viewitems()-TmpDictB.viewitems()
+        TmpDifferB=TmpDictB.viewitems()-TmpDictA.viewitems()
+        print ('+++ echo 文件内容检查结果：'+fileA+' && '+fileB+' #####')
+        for item in TmpDifferA:
+            print (fileA+u'内容异常：'+item[0])
+
+        for item in TmpDifferB:
+            print (fileB+u'内容异常：'+item[0])
+        print ('+++++++++   END    #######')
+
+
+
+    def differFolderStructure(self):
+        TmpPathSep=os.sep
+        self.SrcDirectoriesSet=set()
+        self.SrcFilesSet=set()
+        self.TargetDirectoriesSet=set()
+        self.TargetFilesSet=set()
+        self.CommonFilesSet=set()
+
+        #### 提取source文件夹下面的目录结构  ###
+        for TmpBasePath,TmpSubDirectoriesList,TmpFilesList in walk(self.BaseSrcDir):
+            for TmpSubDirectory in TmpSubDirectoriesList:
+                TmpPath=join(TmpBasePath,TmpSubDirectory)
+                TmpPath=TmpPath.replace(TmpPathSep,'/')
+                TmpPath=TmpPath.replace(self.BaseSrcDir,'',1)
+                self.SrcDirectoriesSet.add(TmpPath)
+
+            for TmpFile in TmpFilesList:
+                TmpPath=join(TmpBasePath,TmpFile)
+                TmpPath=TmpPath.replace(TmpPathSep,'/')
+                TmpPath=TmpPath.replace(self.BaseSrcDir,'',1)
+                self.SrcFilesSet.add(TmpPath)
+
+        #### 提取target文件夹下面的目录结构  ###
+        for TmpBasePath,TmpSubDirectoriesList,TmpFilesList in walk(self.BaseTargetDir):
+            for TmpSubDirectory in TmpSubDirectoriesList:
+                TmpPath=join(TmpBasePath,TmpSubDirectory)
+                TmpPath=TmpPath.replace(TmpPathSep,'/')
+                TmpPath=TmpPath.replace(self.BaseTargetDir,'',1)
+                self.TargetDirectoriesSet.add(TmpPath)
+
+            for TmpFile in TmpFilesList:
+                TmpPath=join(TmpBasePath,TmpFile)
+                TmpPath=TmpPath.replace(TmpPathSep,'/')
+                TmpPath=TmpPath.replace(self.BaseTargetDir,'',1)
+                self.TargetFilesSet.add(TmpPath)
+
+        TmpSetA=self.SrcDirectoriesSet-self.TargetDirectoriesSet
+        TmpSetB=self.TargetDirectoriesSet-self.SrcDirectoriesSet
+        print ('++++++ 检查目录结构   ++++++')
+        for item in TmpSetA:
+            print ('文件夹异常:'+join(self.BaseSrcDir,item))
+        for item in TmpSetB:
+            print ('文件夹异常:'+join(self.BaseTargetDir,item))
+        print ('+++++++ 目录结构检查完毕++++++\n')
+
+        TmpSetA=self.SrcFilesSet-self.TargetFilesSet
+        TmpSetB=self.TargetFilesSet-self.SrcFilesSet
+        print ('++++ 检查文件路径结构 ###')
+        for item in TmpSetA:
+            print ('文件异常：'+join(self.BaseSrcDir,item))
+        for item in TmpSetB:
+            print ('文件异常:'+join(self.BaseTargetDir,item))
+        print ('+++++ 文件路径结构检查完毕.+++++')
+
+        self.CommonFilesSet=self.SrcFilesSet & self.TargetFilesSet
+        for subfile in self.CommonFilesSet:
+            self.differFileContent(join(self.BaseSrcDir,subfile),join(self.BaseTargetDir,subfile))
+
+
+
+
+tmpObj=MavenConfigDiffer()
+tmpObj.differFolderStructure()
+
+
